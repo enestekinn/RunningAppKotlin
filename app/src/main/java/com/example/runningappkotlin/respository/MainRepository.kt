932f76f25a69c1385d4ab19478726313a74dd668
Enes Tekin <derewerq12@hotmail.com>
2020-09-04 22:20:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/runningappkotlin/respository/MainRepository.kt
@@ -0,0 +1,35 @@
+package com.example.runningappkotlin.respository
+
+import com.example.runningappkotlin.db.Run
+import com.example.runningappkotlin.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    val runDAO: RunDAO
+) {
+    suspend fun insertRun(run: Run) = runDAO.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDAO.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDAO.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByDistance() = runDAO.getAllRunsSortedByDistance()
+
+    fun getAllRunsSortedByTimeInMillis() = runDAO.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedByAvgSpeed() = runDAO.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDAO.getAllRunsSortedByCaloriesBurned()
+
+    fun getTotalAvgSpeed() = runDAO.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDAO.getTotalDistance()
+
+    fun getTotalTimeInMillis() = runDAO.getTotalTimeInMillis()
+
+    fun getTotalCaloriesBurned() = runDAO.getTotalCaloriesBurned()
+
+
+
+
+}
